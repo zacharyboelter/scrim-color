cb3efe3b21765465f65020fa3c94dd6a7890f9fd
Zachary Boelter <zboelter@gmail.com>
2022-04-22 13:15:08 -0500

    js get colors and display

---
+++ app.js
@@ -0,0 +1,25 @@
+
+function displayColors(colors){
+    let myColorsHtml = colors.map(color => {
+        return `<div class="my-color" style="background-color: ${color.value}">${color.value}</div>`
+    }).join('')
+    
+    console.log(myColorsHtml)
+
+    document.body.innerHTML = `<div class="my-colors">
+        ${myColorsHtml}
+    </div>`
+}
+
+
+async function getColor() {
+    let response = await fetch('https://apis.scrimba.com/hexcolors/?count=5')
+    let data = await response.json()
+    let colors = data.colors
+    console.log(data)
+
+    displayColors(colors)
+
+}
+
+getColor()
